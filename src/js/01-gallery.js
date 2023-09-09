c65655aafa08746e3ac10811164f013a85467db0
Vladyslav <vladislav2002vladislav2002@gmail.com>
2023-09-09 15:18:06 +0300

    tasks-1-and-3-done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+gallery.addEventListener('click', handleClick);
+
+const markupGallery = createGallery(galleryItems);
+gallery.insertAdjacentHTML("beforeend", markupGallery);
+
+function createGallery (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => { 
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+      />
+    </a>
+    </li>`
+}).join("");
+}
+
+function handleClick(event) {
+    event.preventDefault();
+
+    if (event.target === event.currentTarget) {
+        return;
+    }
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector(".feedback-form");
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageTextarea = feedbackForm.querySelector('textarea[name="message"]');
+
+feedbackForm.addEventListener("input", throttle(handleInput, 500));
+
+window.addEventListener("load", () => {
+    const savedState = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+    emailInput.value = savedState.email || "";
+    messageTextarea.value = savedState.message || "";
+  });
+  
+feedbackForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.currentTarget;
+  const email = emailInput.value;
+  const message = messageTextarea.value;
+  
+  if (email === "" || message === "") {
+    return console.log("Please fill in all the fields!");
+  }
+
+  const formData = {
+    email,
+    message,
+  };
+
+  localStorage.removeItem(LOCALSTORAGE_KEY); 
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+  console.log(formData);
+
+  emailInput.value = "";
+  messageTextarea.value = "";
+}
+
+function handleInput() {
+    const email = emailInput.value;
+    const message = messageTextarea.value;
+    const formData = {
+      email,
+      message,
+    };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+  }
+  
+  handleInput();
 
